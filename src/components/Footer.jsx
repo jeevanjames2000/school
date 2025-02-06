84ade5d912241b5076e529d20ffb21e1f09c4453
jeevan <jeevanjames2000@gmail.com>
2025-02-06 09:59:40 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,22 @@ import {
   FaHeart,
   FaArrowUp,
 } from "react-icons/fa";
+
 export default function Footer({ scrollToSection }) {
   return (
     <footer className="bg-gray-900 text-gray-300 py-10 ">
-      <div className="container mx-auto px-6 lg:px-16 flex flex-col gap-8 md:grid md:grid-cols-3 items-center justify-items-center">
+      <div className="container mx-auto px-6 lg:px-16 flex flex-col gap-8 md:grid md:grid-cols-3 ">
         {}
-        <div className="text-center md:text-left">
+        <div className="text-left">
           <h2 className="text-lg font-bold text-blue-400">SGLF</h2>
           <p className="mt-2 text-sm">
             Nurturing young minds with care, creativity, and compassion since
             2020.
           </p>
         </div>
+
         {}
-        <div className="text-center md:text-left">
+        <div className=" text-left">
           <h2 className="text-lg font-bold">Quick Links</h2>
           <ul className="mt-2 space-y-2">
             <li>
@@ -56,26 +58,29 @@ export default function Footer({ scrollToSection }) {
             </li>
           </ul>
         </div>
+
         {}
-        <div className="text-center md:text-left">
+        <div className="text-left">
           <h2 className="text-lg font-bold">Connect With Us</h2>
           <ul className="mt-2 space-y-2">
-            <li className="flex justify-center md:justify-start items-center gap-2">
+            <li className="flex justify-start md:justify-start items-center gap-2">
               <FaPhoneAlt /> +91 1234567890
             </li>
-            <li className="flex justify-center md:justify-start items-center gap-2">
+            <li className="flex justify-start gap-2">
               <FaEnvelope /> info@sriganeshlittlefeets.edu.in
             </li>
-            <li className="flex justify-center md:justify-start items-center gap-2">
+            <li className="flex justify-start md:justify-start items-center gap-2">
               <FaMapMarkerAlt /> Chennai, Tamil Nadu, India
             </li>
           </ul>
         </div>
       </div>
+
       {}
       <div className="text-center text-sm border-t border-gray-700 mt-8 pt-4">
         Made with <FaHeart className="inline text-red-500" /> by SGLF © 2025
       </div>
+
       {}
       <div className="fixed bottom-5 right-5">
         <div
